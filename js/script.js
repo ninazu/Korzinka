2f139177f77a5e661b6f3d8d7ce96cd76bba3b0a
Farid <faridnagiyev99@gmail.com>
2019-01-17 20:03:40 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Слайдер спец. предложений
+$('.main-slider').slick({
+	infinite: true,
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+  	arrows: false,
+	draggable: false,
+	autoplay: true,
+	autoplaySpeed: 4000,
+	pauseOnFocus: false,
+	dots: true
+});
