5cdf52ba9cf2caa2915c709c72b587c3af2780b6
Farid <faridnagiyev99@gmail.com>
2019-01-17 21:54:25 +0300

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ $('.main-slider').slick({
 	pauseOnFocus: false,
 	dots: true
 });
+
+// Фиксированная "шапка"
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 1) {
+		$('.header-bottom').addClass("sticky");
+	} else {
+		$('.header-bottom').removeClass("sticky");
+	}
+});
+
+// Плавные "якоря"
+$(document).ready(function () {
+	$("#logo, #main-menu, #scroll-top").on("click", "a", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href');
+		var	top = $(id).offset().top
+		$('body,html').animate({
+			scrollTop: top
+		}, 1000);
+	});
+});
